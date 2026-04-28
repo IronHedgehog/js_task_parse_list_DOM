e301bdc6ecf4170f2db5fb82641cf5a905f320c9
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-04-28 14:40:44 +0300

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write code here
+const employeeList = document.querySelector('ul');
+const employeeItems = Array.from(document.querySelectorAll('ul > li'));
+
+const normalizeSalary = (salary) => {
+  let result = '';
+
+  for (const ch of salary) {
+    if (!isNaN(ch)) {
+      result += ch;
+    }
+  }
+
+  return +result;
+};
+
+const sortList = (list) => {
+  return list.sort(
+    (a, b) =>
+      normalizeSalary(b.dataset.salary) - normalizeSalary(a.dataset.salary),
+  );
+};
+
+const sortedList = sortList(employeeItems);
+
+const getEmployees = (list) => {
+  employeeList.innerHTML = '';
+  list.forEach((li) => employeeList.append(li));
+};
+
+getEmployees(sortedList);
